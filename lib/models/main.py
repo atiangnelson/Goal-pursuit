8207e3b1c0944311db38908ca9fa5f05fcbd0b2c
Nelson <nelson.atiang@student.moringaschool.com>
2025-05-30 16:44:45 +0300

    added the help list for my cli interface

---
+++ lib/models/main.py
@@ -0,0 +1,11 @@
+from lib.models.cli import list_goals, add_goal, complete_goal, delete_goal
+
+def print_help():
+    print("""
+Commands:
+  add [title]        - Add a goal
+  list               - List all goals
+  complete [id]      - Mark goal as complete
+  delete [id]        - Delete a goal
+  exit               - Exit app
+""")
